47d8da33db5bac11a7331672be9fcb2252fb6a49
denys-t <tulipadanys@gmail.com>
2022-03-22 11:39:52 +0200

    script.js: added function computerPlay

---
+++ script.js
@@ -0,0 +1,13 @@
+let cpuMove = computerPlay();
+
+console.log(cpuMove);
+
+function computerPlay() {
+    let cpuMoveNum = Math.ceil(Math.random() * 3);
+    switch (cpuMoveNum) {
+        case 1: return "Rock";
+        case 2: return "Paper";
+        case 3: return "Scissors";
+        default: return "Error";
+    }        
+}
