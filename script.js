61baa731f5ede2f2683bf72726457f003020af0f
denys-t <tulipadanys@gmail.com>
2022-03-22 11:42:25 +0200

    script.js: added function playerPlay

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 let cpuMove = computerPlay();
+let playerMove = playerPlay();
 
 console.log(cpuMove);
+console.log(playerMove);
 
 function computerPlay() {
     let cpuMoveNum = Math.ceil(Math.random() * 3);
@@ -10,4 +12,18 @@ function computerPlay() {
         case 3: return "Scissors";
         default: return "Error";
     }        
+}
+
+function playerPlay() {
+    let playerMoveLtr = window.prompt("Choose your move: R = rock, P = paper, S = scissors").toUpperCase();
+    switch (playerMoveLtr) {
+        case "R": return "Rock";
+        case "P": return "Paper";
+        case "S": return "Scissors";
+        default: return "Error";
+    }        
+}
+
+function playRound(playerSelection, computerSelection) {
+
 }
